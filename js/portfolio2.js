0c9f6cb0771ee1d341bb897d8463caa83ecbabd3
BenABaron <benabaron@gmail.com>
2020-11-29 20:39:42 -0600

    Portfolio page finished

---
+++ js/portfolio2.js
@@ -0,0 +1,19 @@
+const OpenWindow = (id) => {
+  if (id === "Calc") {
+    window.open("https://benabaron.github.io/calculator-app-101/", "_blank")
+  }
+  else if (id === "Gallery") {
+    window.open("http://127.0.0.1:5501/views/image-gallery.html", "_blank")
+  }
+  else if (id === "Nasa") {
+    window.open("https://benabaron.github.io/NASA_landing_v2/", "_blank")
+  }
+  else if (id === "Respond") {
+    window.open("https://benabaron.github.io/First-Responsive-Website/", "_blank")
+  }
+  else if (id === "TTT") {
+    window.open("https://benabaron.github.io/TicTacToe-101/", "_blank")
+  }
+  else {
+  }
+}
